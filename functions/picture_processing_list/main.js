d26508a683119a3b1a0b436f8801ee0d05d82d03
Noverish Harold <embrapers263@gmail.com>
2018-08-22 18:13:46 +0900

    Finished picture_processing_list

---
+++ functions/picture_processing_list/main.js
@@ -0,0 +1,30 @@
+const utils = require('../../utils/utils.js');
+const response = require('../../utils/response.js');
+const picture_util = require('../../utils/picture_util.js');
+const mysql = require('mysql');
+const format = require('string-format');
+format.extend(String.prototype);
+
+exports.handle = function (e, ctx, cb) {
+    const conn = mysql.createConnection(utils.mysql_config);
+    const params = utils.process_input_event(e, cb, []);
+    if (params == null)
+        return;
+
+    get();
+
+    function get() {
+        let sql = 'SELECT * FROM Picture WHERE status LIKE \'processing\'';
+        sql = sql.format(params);
+
+        conn.query(sql, [], function (err, results, fields) {
+            if (err) {
+                response.end(cb, 500, err, conn);
+                return;
+            }
+
+            let picture_list = picture_util.process_picture_list(results);
+            response.end(cb, 200, picture_list, conn);
+        });
+    }
+};
