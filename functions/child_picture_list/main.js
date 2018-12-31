2c7f2e37c6d381cf78ba2f36f766f3db272fa18f
Noverish Harold <embrapers263@gmail.com>
2018-12-31 22:08:21 +0900

    2018-12-31 22:08:19

---
+++ functions/child_picture_list/main.js
@@ -0,0 +1,33 @@
+const utils = require('../../utils/utils.js');
+const response = require('../../utils/response.js');
+const album_util = require('../../utils/album_util.js');
+const picture_util = require('../../utils/picture_util.js');
+const mysql = require('mysql');
+const format = require('string-format');
+format.extend(String.prototype);
+
+exports.handle = function (e, ctx, cb) {
+    const conn = mysql.createConnection(utils.mysql_config);
+    const params = utils.process_input_event(e, cb, ['album_id']);
+    if (params == null)
+        return;
+
+    album_util.check_album_exist(params['album_id'], conn, cb, function () {
+        get();
+    });
+
+    function get() {
+        let sql = 'SELECT * FROM ViewPicture WHERE album_id = \'{album_id}\'';
+        sql = sql.format(params);
+
+        conn.query(sql, [], function (err, results, fields) {
+            if (err) {
+                response.end(cb, 500, err, conn);
+                return;
+            }
+
+            let picture_list = picture_util.process_picture_list(results);
+            response.end(cb, 200, picture_list, conn);
+        });
+    }
+};
